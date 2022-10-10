36f28a554db330fd490ca92797a9317fbb56173a
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-10-10 21:06:03 +0700

    finish section 06

---
+++ async-code/async/async-example.test.js
@@ -0,0 +1,26 @@
+import { it, expect } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example";
+
+it("should generate a token value", (done) => {
+  const testUserEmail = "test@test.com";
+  generateToken(testUserEmail, (err, token) => {
+    try {
+      expect(token).toBeDefined();
+      //   expect(token).toBe(2);
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+});
+
+it("should generate a token value", () => {
+  const testUserEmail = "test@test.com";
+  return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+});
+
+it("should generate a token value 2", async () => {
+  const testUserEmail = "test@test.com";
+  const token = await generateTokenPromise(testUserEmail);
+  expect(token).toBeDefined();
+});
